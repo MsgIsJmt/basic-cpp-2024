7c2d8b24c39e353dfc13d4c85657f36bb61a404b
MsgIsJmt <tnwls43888@naver.com>
2024-02-29 17:29:31 +0900

    다음에 해야지...

---
+++ day07/day07/229_8교시.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+class Myclass {
+private:
+	char name[30];
+	int age;
+};
+
+class MyInfo : public Myclass {
+private:
+	char pNumber[20];
+public:
+	void printMyinfo() {
+		"나의 이름은 수진이고 나이는 25살이며 전화번호는 112입니다.";
+	}
+};
+
+int main(void)
+{
+	cout << MyInfo << endl;
+	return 0;
+}
+
+
+// MyInfo 객체를 생성한다.
+// MyInfo개체.printMyInfo();를 호출한다.
